7fd4d87ac6cdc1cb416b940c4d9e6e83309d6d00
Martin Multhaupt <m.multhaupt@setlog.com>
2019-12-04 16:16:39 +0100

    Do not panic when encountering a commit which untracked a file

--- git.go
+++ git.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
 	"io"
 	"os/exec"
 	"path/filepath"
@@ -12,9 +11,7 @@ import (
 
 // returns most recent commit hash where SHA-256 of file rootPath/relPath matches sha
 func commitForSha(repoRoot, rootPath, relPath, sha string) string {
-	// fmt.Println("commitForSha", rootPath, relPath, sha)
 	shas := shasOfFile(repoRoot, rootPath, relPath)
-	// fmt.Println("SHAs of file", filepath.Join(rootPath, relPath), "=", shas)
 	for contentSha, commit := range shas {
 		if sha == contentSha {
 			return commit
@@ -40,7 +37,6 @@ func gitRoot(atPath string) string {
 // map-key is SHA-256 of file content; map-value is commit SHA
 func shasOfFile(repoRoot, rootPath, relPath string) map[string]string {
 	fileCommits := getCommitsForFile(repoRoot, rel(repoRoot, filepath.Join(rootPath, relPath)))
-	// fmt.Println("Commits of file", filepath.Join(rootPath, relPath), "=", fileCommits)
 	m := make(map[string]string)
 	for _, commit := range fileCommits {
 		cmd := exec.Command("git", "show", commit+":"+rel(repoRoot, filepath.Join(rootPath, relPath)))
@@ -63,11 +59,10 @@ func shasOfFile(repoRoot, rootPath, relPath string) map[string]string {
 			panic(err)
 		}
 		err = cmd.Wait()
-		if err != nil {
-			fmt.Println("git show", commit+":"+relPath, "in", cmd.Dir)
-			panic(err)
+		if err == nil {
+			// err will be != nil for example if the file was removed, i.e. became untracked with this commit
+			m[hex.EncodeToString(hash.Sum(nil))] = commit
 		}
-		m[hex.EncodeToString(hash.Sum(nil))] = commit
 	}
 	return m
 }
@@ -76,15 +71,12 @@ func getCommitsForFile(repoRoot, repoRelPath string) []string {
 	cmd := exec.Command("git", "log", "--pretty=tformat:\"%H\"", repoRelPath)
 	var err error
 	cmd.Dir = repoRoot
-	// fmt.Println("Run", cmd.Path, cmd.Args, "in", cmd.Dir)
 	output, err := cmd.Output()
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Println("output=", string(output))
 	commits := strings.FieldsFunc(strings.ReplaceAll(string(output), `"`, ``), func(r rune) bool {
 		return r == '\n'
 	})
-	// fmt.Println("commits=", commits)
 	return commits
 }
